d47e64a3e3f2673fb20b99d81bc815b9f090bb29
0xMDIV <0x.mdiv@gmail.com>
2022-04-30 15:50:46 +0200

    Add basic quote functions

--- commands/Quotes.js
+++ commands/Quotes.js
@@ -4,7 +4,21 @@
 
 class Quotes {
     static quote(ctx) {
-        let args = ctx.message.substring(5);
+        const command_length = 7;
+        let args = ctx.message.text.substring(command_length).split(' ').filter(elem => {
+            return elem != '';
+        })
+        console.log(args, args.length)
+
+        if (args.length === 0) {
+            // select random quote
+        } else if (args.length > 0) {
+           console.log('args bigger as 0')
+        } else {
+            return ctx.reply(`Wrong Command Layout, try: /quote %SEARCH% <author>`);
+        }
 
     }
 }
+
+module.exports = {Quotes}
